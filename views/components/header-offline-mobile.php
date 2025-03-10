181a7e3c5b70ab4d5010488e7f4f1c5210a6994d
Elysio3 <mael.kerviche@outlook.com>
2025-03-10 16:36:11 +0100

    comments wall page for first public deploy

--- views/components/header-offline-mobile.php
+++ views/components/header-offline-mobile.php
@@ -1,153 +1,160 @@
-  
+<style>
+  .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+  }
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
+  .b-example-divider {
+    width: 100%;
+    height: 3rem;
+    background-color: rgba(0, 0, 0, .1);
+    border: solid rgba(0, 0, 0, .15);
+    border-width: 1px 0;
+    box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+  }
 
-      .b-example-divider {
-        width: 100%;
-        height: 3rem;
-        background-color: rgba(0, 0, 0, .1);
-        border: solid rgba(0, 0, 0, .15);
-        border-width: 1px 0;
-        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-      }
+  .b-example-vr {
+    flex-shrink: 0;
+    width: 1.5rem;
+    height: 100vh;
+  }
 
-      .b-example-vr {
-        flex-shrink: 0;
-        width: 1.5rem;
-        height: 100vh;
-      }
+  .bi {
+    vertical-align: -.125em;
+    fill: currentColor;
+  }
 
-      .bi {
-        vertical-align: -.125em;
-        fill: currentColor;
-      }
+  .nav-scroller {
+    position: relative;
+    z-index: 2;
+    height: 2.75rem;
+    overflow-y: hidden;
+  }
 
-      .nav-scroller {
-        position: relative;
-        z-index: 2;
-        height: 2.75rem;
-        overflow-y: hidden;
-      }
+  .nav-scroller .nav {
+    display: flex;
+    flex-wrap: nowrap;
+    padding-bottom: 1rem;
+    margin-top: -1px;
+    overflow-x: auto;
+    text-align: center;
+    white-space: nowrap;
+    -webkit-overflow-scrolling: touch;
+  }
 
-      .nav-scroller .nav {
-        display: flex;
-        flex-wrap: nowrap;
-        padding-bottom: 1rem;
-        margin-top: -1px;
-        overflow-x: auto;
-        text-align: center;
-        white-space: nowrap;
-        -webkit-overflow-scrolling: touch;
-      }
+  .btn-bd-primary {
+    --bd-violet-bg: #712cf9;
+    --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
 
-      .btn-bd-primary {
-        --bd-violet-bg: #712cf9;
-        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
+    --bs-btn-font-weight: 600;
+    --bs-btn-color: var(--bs-white);
+    --bs-btn-bg: var(--bd-violet-bg);
+    --bs-btn-border-color: var(--bd-violet-bg);
+    --bs-btn-hover-color: var(--bs-white);
+    --bs-btn-hover-bg: #6528e0;
+    --bs-btn-hover-border-color: #6528e0;
+    --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+    --bs-btn-active-color: var(--bs-btn-hover-color);
+    --bs-btn-active-bg: #5a23c8;
+    --bs-btn-active-border-color: #5a23c8;
+  }
 
-        --bs-btn-font-weight: 600;
-        --bs-btn-color: var(--bs-white);
-        --bs-btn-bg: var(--bd-violet-bg);
-        --bs-btn-border-color: var(--bd-violet-bg);
-        --bs-btn-hover-color: var(--bs-white);
-        --bs-btn-hover-bg: #6528e0;
-        --bs-btn-hover-border-color: #6528e0;
-        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-        --bs-btn-active-color: var(--bs-btn-hover-color);
-        --bs-btn-active-bg: #5a23c8;
-        --bs-btn-active-border-color: #5a23c8;
-      }
+  .bd-mode-toggle {
+    z-index: 1500;
+  }
 
-      .bd-mode-toggle {
-        z-index: 1500;
-      }
+  .bd-mode-toggle .dropdown-menu .active .bi {
+    display: block !important;
+  }
+</style>
 
-      .bd-mode-toggle .dropdown-menu .active .bi {
-        display: block !important;
-      }
-    </style>
+<style>
+  /* Style adjustments for mobile screens */
+  @media (max-width: 1400px) {
+    .navbar-brand img {
+      width: 150px;
+      /* Increase the logo size */
+      height: auto;
+      /* Keep aspect ratio */
+    }
 
-    <style>
-        /* Style adjustments for mobile screens */
-        @media (max-width: 1400px) {
-            .navbar-brand img {
-                width: 150px;   /* Increase the logo size */
-                height: auto;   /* Keep aspect ratio */
-            }
+    .navbar {
+      padding: 7px;
+      /* Increase the padding of the navbar */
+    }
 
-            .navbar {
-                padding: 7px;  /* Increase the padding of the navbar */
-            }
+    .nav-link {
+      font-size: 2rem;
+      /* Increase font size for the nav items */
+    }
 
-            .nav-link {
-                font-size: 2rem;  /* Increase font size for the nav items */
-            }
+    .navbar-toggler {
+      font-size: 2rem;
+      /* Make the toggle button larger */
+      padding: 5px 7px;
+      /* Increase the button padding */
+    }
 
-            .navbar-toggler {
-                font-size: 2rem;  /* Make the toggle button larger */
-                padding: 5px 7px;  /* Increase the button padding */
-            }
-
-            .offcanvas-header h5 {
-                font-size: 1.5rem;  /* Increase the size of the offcanvas title */
-            }
-        }
-    </style>
+    .offcanvas-header h5 {
+      font-size: 1.5rem;
+      /* Increase the size of the offcanvas title */
+    }
+  }
+</style>
 
 </head>
 mobile
+
 <body>
-<main>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Offcanvas navbar large">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="/">
-      <img src="/public/images/2021-3MlogoSimple-2048x866.png" alt="Les 3 Mousquetons" width="100" height="auto">
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
-        <div class="offcanvas-header">
-          <h5 class="offcanvas-title" id="offcanvasNavbar2Label">Les 3 mousquetons</h5>
-          <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-        </div>
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+  <main>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Offcanvas navbar large">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <img src="/public/images/2021-3MlogoSimple-2048x866.png" alt="Les 3 Mousquetons" width="100" height="auto">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2"
+          aria-controls="offcanvasNavbar2" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasNavbar2"
+          aria-labelledby="offcanvasNavbar2Label">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbar2Label">Les 3 mousquetons</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+              aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
 
-            <li class="nav-item">
-              <a class="nav-link btn btn-primary" href="/?page=login">Se Connecter</a>
-            </li>
+              <li class="nav-item">
+                <a class="nav-link btn btn-primary" href="/?page=login">Se Connecter</a>
+              </li>
 
-            <hr>
-            
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/?page=home">Accueil</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/?page=walls">Les Murs</a>
-            </li>
+              <hr>
 
-            
-          </ul>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/?page=home">Accueil</a>
+              </li>
+              <li class="nav-item">
+                <!--<a class="nav-link" href="/?page=walls">Les Murs</a>-->
+              </li>
 
-      
-          
+
+            </ul>
+
+
+
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
-</main>
-
-
+    </nav>
+  </main>
